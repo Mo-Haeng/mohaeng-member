42d2adcd28d7c0fad3a65a2d230ee2e699bb9f99
shinD <huipulci1@naver.com>
2022-08-30 01:25:35 +0900

    [FEAT] Eureka Client 등록

---
+++ mohang-member-core/src/main/kotlin/com/mohang/config/EurekaClientConfig.kt
@@ -0,0 +1,12 @@
+package com.mohang.config
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Created by ShinD on 2022/08/30.
+ */
+@EnableDiscoveryClient
+@Configuration
+class EurekaClientConfig {
+}
