c25c7b90c17c746faae253db1f0bdda97bbec711
shinD <huipulci1@naver.com>
2022-08-30 02:09:49 +0900

    [REFACTOR] Eureka Config 클래스 패키지 수정

--- mohang-member-core/src/main/kotlin/com/mohang/config/eureka/EurekaClientConfig.kt
+++ mohang-member-core/src/main/kotlin/com/mohang/config/eureka/EurekaClientConfig.kt
@@ -1,4 +1,4 @@
-package com.mohang.config
+package com.mohang.config.eureka
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.Configuration
