5667b9cb07f5e9f3327a97368f8200ba19fd03d9
shinD <huipulci1@naver.com>
2022-08-30 15:50:42 +0900

    [CHORE] 멀티모듈 프로젝트 제거, 단일 모듈로 초기화.
    - 우선 단일 모듈로 개발한 이후 멀티 모듈로 점점 바꾸기

--- mohang-member-core/src/main/kotlin/com/mohang/application/exception/DuplicateUsernameException.kt
+++
@@ -1,9 +0,0 @@
-package com.mohang.application.exception
-
-/**
- * Created by ShinD on 2022/08/30.
- */
-class DuplicateUsernameException : RuntimeException(
-    "이미 존재하는 아이디가 있습니다. 다른 아이디를 입력해주세요"
-) {
-}
--- mohang-member-core/src/main/kotlin/com/mohang/infrastructure/persistence/Persistence.kt
+++
@@ -1,7 +0,0 @@
-package com.mohang.infrastructure.persistence
-
-/**
- * Created by ShinD on 2022/08/30.
- */
-interface Persistence {
-}
