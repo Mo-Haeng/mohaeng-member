430020c1ca1472e3f0011ca0cc76af9f408969a5
shinD <huipulci1@naver.com>
2022-08-30 19:20:12 +0900

    [FEAT] [#1] 임시 실행을 위한 MockEncoder(MemberPasswordEncoder) 구현

---
+++ src/main/kotlin/com/mohang/domain/member/MockEncoder.kt
@@ -0,0 +1,17 @@
+package com.mohang.domain.member
+
+import org.springframework.stereotype.Service
+
+/**
+ * Created by ShinD on 2022/08/30.
+ */
+class MockEncoder : MemberPasswordEncoder {
+
+    override fun encode(rawPassword: String): String {
+        return "!!"
+    }
+
+    override fun matches(rawPassword: String, encodedPassword: String): Boolean {
+        TODO("Not yet implemented")
+    }
+}
