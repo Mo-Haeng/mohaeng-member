d38daad5c6408c8388db417adc7a4396bce17812
shinD <huipulci1@naver.com>
2022-08-30 19:16:02 +0900

    [TEST] [#1] 테스트를 위한 MockTransactionTemplate 생성

---
+++ src/test/kotlin/com/mohang/presentation/MockTransactionTemplate.kt
@@ -0,0 +1,15 @@
+package com.mohang.presentation
+
+import org.springframework.transaction.support.SimpleTransactionStatus
+import org.springframework.transaction.support.TransactionCallback
+import org.springframework.transaction.support.TransactionTemplate
+
+/**
+ * Created by ShinD on 2022/08/30.
+ */
+class MockTransactionTemplate : TransactionTemplate() {
+
+    override fun <T> execute(action: TransactionCallback<T>): T? {
+        return action.doInTransaction(SimpleTransactionStatus())
+    }
+}
