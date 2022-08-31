1abcf0f8aa60bdf884fe8cd017f08998a4113b03
shinD <huipulci1@naver.com>
2022-08-31 23:04:09 +0900

    [FEAT] [#1] 임시 비밀번호 인코더 생성

--- src/main/kotlin/com/mohang/domain/member/MockEncoder.kt
+++ src/main/kotlin/com/mohang/domain/member/MockEncoder.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service
 /**
  * Created by ShinD on 2022/08/30.
  */
+@Service
 class MockEncoder : MemberPasswordEncoder {
 
     override fun encode(rawPassword: String): String {
