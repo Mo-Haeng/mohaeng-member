d887eb1f7f84b36af46dbe64996b774e1c7b0aca
shinD <huipulci1@naver.com>
2022-09-02 17:31:50 +0900

    [FEAT] [#1] MemberDetails에 equals랑 hashcode 추가

--- src/main/kotlin/com/mohang/infrastructure/authentication/jsonlogin/userdetails/MemberDetails.kt
+++ src/main/kotlin/com/mohang/infrastructure/authentication/jsonlogin/userdetails/MemberDetails.kt
@@ -16,8 +16,28 @@ class MemberDetails(
     password: String,
 
     val role: Role,
+
 ) : User(
     username,
     password,
     listOf(SimpleGrantedAuthority(role.authority)) //SimpleGrantedAuthority(ROLE_XXX)) 이런 식으로 사용함
-)
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is MemberDetails) return false
+        if (!super.equals(other)) return false
+
+        if (id != other.id) return false
+        if (role != other.role) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + id.hashCode()
+        result = 31 * result + role.hashCode()
+        return result
+    }
+}
