687831e1df635791b17a4ad70d8113a20bc20e41
shinD <huipulci1@naver.com>
2022-09-02 20:12:41 +0900

    [TEST] [#1] MemberDetails Test

--- src/test/kotlin/com/mohang/infrastructure/authentication/jsonlogin/userdetails/MemberDetailsTest.kt
+++ src/test/kotlin/com/mohang/infrastructure/authentication/jsonlogin/userdetails/MemberDetailsTest.kt
@@ -1,8 +1,60 @@
 package com.mohang.infrastructure.authentication.jsonlogin.userdetails
 
+import com.mohang.domain.enums.Role
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import strikt.assertions.isNull
 
 /**
  * Created by ShinD on 2022/09/02.
  */
-internal class MemberDetailsTest
+internal class MemberDetailsTest {
+
+    @Test
+    fun `MemberDetails 정보 테스트`() {
+
+        //given
+        val memberId = 1L
+        val username = "username"
+        val password = "password"
+        val role = Role.BASIC
+
+        //when
+        val memberDetails = MemberDetails(
+            id = memberId,
+            username = username,
+            password = password,
+            role = role,
+        )
+
+        //then
+        expectThat(memberDetails.id).isEqualTo(memberId)
+        expectThat(memberDetails.username).isEqualTo(username)
+        expectThat(memberDetails.password).isEqualTo(password)
+        expectThat(memberDetails.role).isEqualTo(role)
+    }
+
+    @Test
+    fun `eraseCredentials 테스트`() {
+
+        //given
+        val memberId = 1L
+        val username = "username"
+        val password = "password"
+        val role = Role.BASIC
+        val memberDetails = MemberDetails(
+            id = memberId,
+            username = username,
+            password = password,
+            role = role,
+        )
+
+        //when
+        memberDetails.eraseCredentials()
+
+        //then
+        expectThat(memberDetails.password).isNull()
+    }
+}
