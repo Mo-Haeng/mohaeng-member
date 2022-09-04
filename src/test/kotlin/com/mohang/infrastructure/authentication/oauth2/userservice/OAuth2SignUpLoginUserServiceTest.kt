3e75352dff117d8c78c0681dbad35bbdc0aa8905
shinD <huipulci1@naver.com>
2022-09-04 21:07:05 +0900

    [TEST] [#1] OAuth2SignUpUseCase Test

--- src/test/kotlin/com/mohang/infrastructure/authentication/oauth2/userservice/OAuth2SignUpLoginUserServiceTest.kt
+++ src/test/kotlin/com/mohang/infrastructure/authentication/oauth2/userservice/OAuth2SignUpLoginUserServiceTest.kt
@@ -3,4 +3,17 @@ package com.mohang.infrastructure.authentication.oauth2.userservice
 /**
  * Created by ShinD on 2022/09/01.
  */
-internal class OAuth2SignUpLoginUserServiceTest
+internal class OAuth2SignUpLoginUserServiceTest {
+
+    /**
+     * OAuth2SignUpLoginUserService
+     *
+     * DefaultOAuth2UserService의 loaduser를 부르는가
+     *
+     * OAuth2AttributesToMemberConverter convert 호출
+     *
+     * signUpUseCase.command 호출
+     *
+     * AuthMemberPrinciple 반화
+     */
+}
--- src/test/kotlin/com/mohang/infrastructure/authentication/oauth2/userservice/usecase/OAuth2SignUpUseCaseTest.kt
+++ src/test/kotlin/com/mohang/infrastructure/authentication/oauth2/userservice/usecase/OAuth2SignUpUseCaseTest.kt
@@ -1,6 +1,78 @@
 package com.mohang.infrastructure.authentication.oauth2.userservice.usecase
 
+import com.mohang.application.member.exception.DuplicateEmailException
+import com.mohang.fixture.MemberFixture
+import com.mohang.infrastructure.persistence.MemberRepository
+import com.mohang.mock.MockTransactionTemplate
+import io.mockk.every
+import io.mockk.mockkClass
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.api.expectThrows
+import strikt.assertions.isEqualTo
+
 /**
  * Created by ShinD on 2022/09/01.
  */
-internal class OAuth2SignUpUseCaseTest
+internal class OAuth2SignUpUseCaseTest {
+
+    /**
+     * 존재하지 않는 경우 회원가입 + 멤버 반환
+     * 존재하는 경우 아무것도 X + 멤버 반환
+     *
+     * 이메일 중복인 경우 예외 발생
+     */
+    private val memberRepository = mockkClass(MemberRepository::class)
+    private val template = MockTransactionTemplate()
+    private val oauth2SignUpUseCase = OAuth2SignUpUseCase(memberRepository, template)
+
+    @Test
+    fun `OAuth2 회원가입 성공`() {
+
+        //given
+        every { memberRepository.findByOauth2LoginId(any()) } returns null
+        every { memberRepository.findByEmail(any()) } returns null
+        val notSavedMember = MemberFixture.notSavedMember()
+        every { memberRepository.save(any()) } returns notSavedMember // 뭐든 상관없음
+
+        //when
+        val savedMember = oauth2SignUpUseCase.command(notSavedMember)
+
+        //then
+        verify (exactly = 1) { memberRepository.save(any()) }
+    }
+
+    @Test
+    fun `이메일 중복 시 회원가입 실패`() {
+
+        //given
+        every { memberRepository.findByOauth2LoginId(any()) } returns null
+        val member = MemberFixture.notSavedMember()
+        every { memberRepository.findByEmail(any()) } returns member
+
+        //when
+        expectThrows<DuplicateEmailException> {
+            //then
+            oauth2SignUpUseCase.command(member)
+        }
+        verify (exactly = 0) { memberRepository.save(any()) }
+    }
+
+    @Test
+    fun `이미 가입된 소셜 계정인 경우 회원가입 진행하지 않고 정보만 반환`() {
+
+        //given
+        val member = MemberFixture.notSavedMember()
+        every { memberRepository.findByOauth2LoginId(any()) } returns member
+
+
+        //when
+        expectThat(oauth2SignUpUseCase.command(member)) {
+            isEqualTo(member)
+        }
+
+        //then
+        verify (exactly = 0){ memberRepository.save(any()) }
+    }
+}
